4c405552115b56ae02eeb5b2f06c81e1e4480583
wunami1010 <61863232+wunami1010@users.noreply.github.com>
2021-04-28 08:02:44 +0800

    新增图表
    新增图表

--- app/src/main.js
+++ app/src/main.js
@@ -9,10 +9,12 @@ import '../static/css/color-dark.css'
 import '../static/css/main.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import echarts from 'echarts'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
+Vue.prototype.$echarts = echarts
 axios.defaults.baseURL = '/api'
 
 /* eslint-disable no-new */
--- app/src/page/essential_i/staff.vue
+++ app/src/page/essential_i/staff.vue
@@ -12,7 +12,11 @@
       </div>
     </div>
     <div class="s_contain">
-      <div class="s_left"></div>
+      <div class="s_left">
+        <div id="sexecharts" :style="{width: '20rem', height: '20rem'}"></div>
+        <div id="departmentecharts" :style="{width: '28rem', height: '28rem'}"></div>
+        <div id="ageecharts" :style="{width: '20rem', height: '20rem'}"></div>
+      </div>
       <div class="s_right">
         <div class="s_rightheader">
           <div>员工列表</div>
@@ -27,10 +31,9 @@
             <div id="searchBtn"><img src="../../../static/ai/搜索.png"></div>
           </div>
         </div>
-        <div>
-          信息排序
-        </div>
-        <el-table :data="tableData" stripe>
+        <el-table
+        :data="tableData.slice((currentPage-1)*pagesize, currentPage*pagesize)"
+        stripe>
           <el-table-column
           prop="jobNum"
           label="工号">
@@ -51,10 +54,20 @@
           prop="tel"
           label="联系方式">
         </el-table-column>
-        <el-table-column>
-            查看详情
+        <el-table-column
+          label="其他信息">
+          ...
         </el-table-column>
         </el-table>
+        <el-pagination
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-size="pagesize"
+          layout="total, prev, pager, next"
+          :total="tableData.length"
+          prev-text="<<"
+          next-text=">>">
+        </el-pagination>
       </div>
     </div>
   </div>
@@ -64,6 +77,8 @@
 export default {
   data () {
     return {
+      currentPage: 1,
+      pagesize: 10,
       search: '',
       searchtype: '姓名',
       sumList: [
@@ -74,17 +89,120 @@ export default {
         {name: '本月离职', num: 0}
       ],
       tableData: [
-        {jobNum: 1, name: '张三', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
-        {jobNum: 2, name: '张三', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
-        {jobNum: 3, name: '张三', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
-        {jobNum: 4, name: '张三', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
-        {jobNum: 5, name: '张三', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
-        {jobNum: 6, name: '张三', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
-        {jobNum: 7, name: '张三', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
-        {jobNum: 8, name: '张三', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
-        {jobNum: 9, name: '张三', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
-        {jobNum: 10, name: '张三', department: '行政部', position: '员工', tel: '138xxxxxxxx'}
+        {jobNum: 1, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 2, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 3, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 4, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 5, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 6, name: '张三', sex: '女', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 7, name: '张三', sex: '女', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 8, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 9, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 10, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 11, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 12, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 13, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 14, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 15, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'},
+        {jobNum: 16, name: '张三', sex: '男', department: '行政部', position: '员工', tel: '138xxxxxxxx'}
       ]
+    }
+  },
+  mounted () {
+    this.drawLines()
+  },
+  methods: {
+    handleCurrentChange (currentPage) {
+      this.currentPage = currentPage
+    },
+    drawLines () {
+      var sexechart = this.$echarts.init(document.getElementById('sexecharts'))
+      var departmentchart = this.$echarts.init(document.getElementById('departmentecharts'))
+      var ageechart = this.$echarts.init(document.getElementById('ageecharts'))
+      sexechart.setOption({
+        title: {
+          text: '员工男女比例',
+          left: 'center'
+        },
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left'
+        },
+        series: [
+          {
+            name: '',
+            type: 'pie',
+            radius: '50%',
+            data: [
+              {value: 31, name: '女'},
+              {value: 60, name: '男'}
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      })
+      departmentchart.setOption({
+        title: {
+          text: '部门人员分布',
+          left: 'center'
+        },
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left'
+        },
+        series: [
+          {
+            name: '',
+            type: 'pie',
+            radius: '50%',
+            data: [
+              {value: 15, name: '行政部'},
+              {value: 9, name: '财务部'},
+              {value: 12, name: '管理部'},
+              {value: 18, name: '技术部'},
+              {value: 12, name: '人力资源部'},
+              {value: 11, name: '客服部'},
+              {value: 15, name: '其他'}
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      })
+      ageechart.setOption({
+        title: {
+          text: '人员年龄分布',
+          left: 'center'
+        },
+        xAxis: {
+          type: 'category',
+          data: ['18-25', '25-30', '30-35', '35-40', '40-']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: ['21', '13', '14', '8', '2'],
+          type: 'bar'
+        }]
+      })
     }
   }
 }
@@ -93,6 +211,9 @@ export default {
 <style scoped>
 .el-breadcrumb{
   margin-bottom: 2rem;
+}
+.el-table{
+  height: 33rem;
 }
 .s_top{
   width: 100%;
@@ -118,13 +239,19 @@ export default {
 .s_left{
   flex: 2;
   height: 46rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
   margin-right: 2.6rem;
   border: 0.08rem solid lightgray;
   box-shadow: 0.1rem 0.1rem 0.3rem rgb(202, 196, 196);
 }
+.s_left div{
+  margin-top: 2rem;
+}
 .s_right{
   flex: 3;
-  height: 46rem;
+  height: 40rem;
   border: 0.08rem solid rgb(202, 191, 191);
   box-shadow: 0.1rem 0.1rem 0.3rem rgb(202, 196, 196);
 }
@@ -134,6 +261,7 @@ export default {
   padding-right: 3rem;
   border-bottom: 0.08rem solid lightgray;
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
   align-items: center;
   text-align: left;
